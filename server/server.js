d6f941c57e4097d2201da10e890dc07b8e326d2f
Abdur Rahman Shafi <abdur.rahman.shafi@g.bracu.ac.bd>
2025-07-30 20:37:29 +0600

    .

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: frontend,
-    credentials: true, 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], 
   })
 );
 app.use(cookieParser());
